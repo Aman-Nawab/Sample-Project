d567c3b4d7b403545a6aac97bd9984fc601628f1
Aman-Nawab <amannawab0227@gmail.com>
2024-11-14 22:07:30 +0530

    changes are made in test.java

--- test.java
+++ test.java
@@ -1,16 +1,8 @@
 import java.util.Scanner;
 
-public class sum {
+public class test {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter two number");
-        int a,b,c;
-        a=sc.nextInt();//here we consider data type as interger but if we will provide float then it will give error 
-        b=sc.nextInt();
-        c=a+b;
-        System.out.println("sum of the "+a+" and "+b+" is "+c);
-
-
+        System.out.println("Here we are writing code so that i can push in hiresh and pull in nilesh");
     }
     
 }
