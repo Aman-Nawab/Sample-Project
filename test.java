eb89ffc32322593be740b147031cfc1b02f934b3
Aman-Nawab <amannawab0227@gmail.com>
2024-11-26 10:38:05 +0530

    Changes made at colleges

--- test.java
+++ test.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         System.out.println("Here we are writing code so that i can push in hiresh and pull in nilesh");
+        System.out.println("i am nawab and learning git and github in college");
+        System.out.println("Hey Aws folk");
     }
     
 }
